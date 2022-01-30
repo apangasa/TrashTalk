88d9de64dfddc2ca1308217749fddd9642169b5d
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-30 01:28:00 -0500

    Change direction based on signal

--- Arduino/receiver/receiver.ino
+++ Arduino/receiver/receiver.ino
@@ -24,16 +24,29 @@ void setup() {
 
 void loop() 
 { 
- // scan from 0 to 180 degrees
-  for(angle = 10; angle < 180; angle++)  
-  {                                  
-    servo.write(angle);               
-    delay(15);                   
-  } 
-  // now scan back from 180 to 0 degrees
-  for(angle = 180; angle > 10; angle--)    
-  {                                
-    servo.write(angle);           
-    delay(15);       
-  } 
+  if(nrf.available()) {
+    char data[1] = "";
+    nrf.read(&data, sizeof(data));
+
+    if(data[0] == '1') {
+       // scan from 0 to 180 degrees
+      for(angle = 10; angle < 180; angle++)  
+      {                                  
+        servo.write(angle);               
+        delay(15);                   
+      } 
+    } else if (data[0] == '0') {
+      
+        // now scan back from 180 to 0 degrees
+        for(angle = 180; angle > 10; angle--)    
+        {                                
+          servo.write(angle);           
+          delay(15);       
+        } 
+    }
+    
+  }
+
+  
+
 }
