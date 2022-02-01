6b14a875b584b4c3552ff0242f0407a96e355947
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-31 20:34:52 -0500

    motor code

--- Arduino/controller/controller.ino
+++ Arduino/controller/controller.ino
@@ -1,9 +1,13 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+//#include <SPI.h>
+//#include <nRF24L01.h>
+//#include <RF24.h>
 
+#include <Stepper.h>
+const int STEPS_PER_REVOLUTION = 2048;  // change this to fit the number of steps per revolution
+int NUM_REVS = 8;
+Stepper myStepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
 
-RF24 nrf(9, 8); // CE, CNS
+//RF24 nrf(9, 8); // CE, CNS
 
 int x;
 
@@ -14,10 +18,13 @@ void setup() {
     Serial.begin(9600);
     while(!Serial) {}
 
-    nrf.begin();
-    nrf.openWritingPipe(address);
-    nrf.setPALevel(RF24_PA_MIN); // power amplifier level
-    nrf.stopListening();
+    myStepper.setSpeed(10);
+
+
+//    nrf.begin();
+//    nrf.openWritingPipe(address);
+//    nrf.setPALevel(RF24_PA_MIN); // power amplifier level
+//    nrf.stopListening();
 }
 
 void loop() {
@@ -26,14 +33,16 @@ void loop() {
 
   if(x == 1) { // open
     Serial.print("Opening...");
-    const char data[] = "1";
-
-    nrf.write(&data, 1);
+    myStepper.step(-1 * NUM_REVS * STEPS_PER_REVOLUTION);
+//    const char data[] = "1";
+//
+//    nrf.write(&data, 1);
   } else if (x == 0) { // close
     Serial.print("Closing...");
-    const char data[] = "0";
-
-    nrf.write(&data, 1);
+    myStepper.step(1 * NUM_REVS * STEPS_PER_REVOLUTION);
+//    const char data[] = "0";
+//
+//    nrf.write(&data, 1);
   }
   
 }
